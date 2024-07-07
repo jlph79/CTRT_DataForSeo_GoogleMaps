5586422b330f58acc1fcf4dc3624016af4d20f05
Pam Scheer <pam.scheer@stichd.com>
2024-07-07 20:06:11 +0200

    replace method

--- data_cleaning.py
+++ data_cleaning.py
@@ -6,7 +6,7 @@ server = 'ctrt-sqlserver.database.windows.net'
 database = 'ctrt-sqldb'
 username = 'CTRT-admin'
 password = 'Hso2Ghana'
-table_name = 'bronze.Population' 
+table_name = 'bronze.PS_Highschools_Accra' 
 port = '1433'  # Default is 1433, e.g., '1433'
 
 # Create the connection string for SQLAlchemy
@@ -25,14 +25,23 @@ query = f"SELECT * FROM {table_name}"
 df = pd.read_sql(query, engine)
 
 # Show the DataFrame
-print(df)
 
+#pd.set_option('display.max_columns', None)
+#pd.reset_option('max_columns')
+
+print(df)
+#w['female'] = w['female'].map({'female': 1, 'male': 0})
 # Add transformations
+
+#REPLACE
+df['rating_value'] = df['rating_value'].replace(4.2, 0)
+print(df[['rating_value']].to_string(index=False)) 
+
 
 new_schema = 'silver'
 new_table_name = 'Population'
 
 # Write DataFrame to the new table in the specified schema in the database
-df.to_sql(new_table_name, engine, schema=new_schema, if_exists='replace', index=False)
+#df.to_sql(new_table_name, engine, schema=new_schema, if_exists='replace', index=False)
 
-print(f"Data written to {new_schema}.{new_table_name} successfully.")
+#print(f"Data written to {new_schema}.{new_table_name} successfully.")
